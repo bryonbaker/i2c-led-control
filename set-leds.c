71b36b73145a5978a2660a6df01627572d13b958
Bryon Baker <bryonbakeraus@gmail.com>
2025-09-30 23:43:12 +1000

    Fixed includes

--- set-leds.c
+++ set-leds.c
@@ -1,21 +1,15 @@
 // ledctl.c
 #define _GNU_SOURCE
-#include <errno.h>
-#include <fcntl.h>
-#include <glob.h>
-#include <linux/i2c-dev.h>
-#include <linux/i2c.h>
-#include <linux/i2c.h>
-#include <linux/i2c-dev.h>
-#include <linux/i2c.h>
-#include <linux/i2c.h>
-#include <linux/i2c-dev.h>
-#include <linux/i2c.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <glob.h>
+#include <linux/i2c-dev.h>
+#include <linux/i2c.h>
 
 #ifndef ARRAY_SIZE
 #define ARRAY_SIZE(a) (sizeof(a)/sizeof((a)[0]))
